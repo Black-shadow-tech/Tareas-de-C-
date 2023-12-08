9892c820e0dfadbb0c8cba29c883cc0ad677889e
Moises Alcantara <148722914+Black-shadow-tech@users.noreply.github.com>
2023-12-08 13:34:38 -0400

    Agregando mas tareas

---
+++ Tarea de curso #7/Numeros primos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros_primos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quieres saber si tu numero es primo o no? digita un numero: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+          
+            if (n %2 == 0 && n > 0)
+            {
+                if (n == 2)
+                {
+                    Console.WriteLine("\nEl {0} es el unico numero primopar.\n",n);
+                }
+                else
+                {
+                    Console.WriteLine("\nEl numero {0} es primo.\n", n);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nEl numero {0} no es primo.\n",n);
+            }
+
+        }
+    }
+}
---
+++ Tarea de curso #8/Calcular la potyencia/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular_la_potyencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite la ptencia: ");
+            int exponete = Convert.ToInt32(Console.ReadLine());
+            int resultado = 1, i;
+
+            Console.Write("Digite la base: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for(i=0; i<exponete; i++)
+            {
+                resultado *= n;
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("   {0}    ",exponete);
+            Console.WriteLine(" ({0}) = {1}  ",n,resultado);
+
+            Console.WriteLine("\n");
+            
+
+
+        }
+    }
+}
